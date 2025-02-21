70b5e456ab0c04009690356224655ede3c9f2589
NataliiaBumbu <130250304+NataliiaBumbu@users.noreply.github.com>
2025-02-21 17:19:58 +0200

    review and uniqueness section

--- src/js/reviews.js
+++ src/js/reviews.js
@@ -1,25 +1,29 @@
-// import Swiper from 'swiper';
-// import 'swiper/css';
-// import { Navigation, Keyboard } from 'swiper/modules';
+import Swiper from 'swiper';
+import 'swiper/css';
+import { Navigation, Keyboard } from 'swiper/modules';
 
-// const swiperReviews = new Swiper('.review-carousel', {
-//   modules: [Navigation, Keyboard],
-//   slidesPerView: 1,
-//   spaceBetween: 20,
-//   loop: true,
-//   navigation: {
-//     nextEl: '.review-next-btn-custom',
-//     prevEl: '.review-prev-btn-custom',
-//   },
-  
-//   keyboard: {
-//     enabled: true,
-//     onlyInViewport: true,
-//     pageUpDown: true,
-//   },
-//   breakpoints: {
-//     768: {
-//       slidesPerView: 2,
-//     }
-//   },
-// });
+const swiperReviews = new Swiper('.review-carousel', {
+  modules: [Navigation, Keyboard],
+  slidesPerView: 1, // 1 картка за замовчуванням
+  spaceBetween: 20,
+  loop: false, // Вимикаємо безкінечний цикл, щоб уникнути багів
+  navigation: {
+    nextEl: '.review-next-btn-custom',
+    prevEl: '.review-prev-btn-custom',
+  },
+  keyboard: {
+    enabled: true,
+    onlyInViewport: true,
+    pageUpDown: true,
+  },
+  breakpoints: {
+    768: {
+      slidesPerView: 2, // 2 картки на планшеті та десктопі
+    }
+  },
+  on: {
+    reachEnd: function () {
+      console.log("Дійшли до останнього слайду");
+    },
+  }
+});
