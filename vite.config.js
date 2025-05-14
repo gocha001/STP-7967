f85d4adfa2d4b91bff8848e37814b92f918bcc0d
gocha001 <kalianichgocha@ukr.net>
2025-05-14 18:44:24 +0300

    fix vite

--- vite.config.js
+++ vite.config.js
@@ -7,6 +7,7 @@ import { globalStylesOptions } from './global.styles';
 
 export default defineConfig(({ command }) => {
   return {
+    base: '/STP-7967/',
     define: {
       [command === 'serve' ? 'global' : '_global']: {},
     },
